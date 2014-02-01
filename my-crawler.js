062eab43b8a3344fb273326fb991a1b6c8fe8323
Rob Colburn <rob@robcolburn.com>
2014-01-31 17:53:27 -0800

    Move to Tarantula, note we need the official Tarantula upgraded

--- my-crawler.js
+++ my-crawler.js
@@ -1,99 +1,63 @@
 #!/usr/bin/env node
-var Crawler = require("crawler").Crawler;
+var Tarantula = require('tarantula');
 var console = require('better-console');
 // 1. Parse the URL from the Arguments
 var argv = require('optimist')
     .usage([
-      'Traverse a site.',
-      'Usage: $0 http://example.com',
-      'Usage: $0 http://example.com -max=10'
-    ].join('\n'))
-    .demand(1)
-    .default('max', 10)
-    .argv;
-var MAX_CONNECTIONS = argv.max;
+			'Traverse a site.',
+			'Usage: $0 http://example.com',
+			'Usage: $0 http://example.com --legs=10',
+			'Usage: $0 http://example.com --legs=10 --proxy=myproxy.com:7070 --web --phantom'
+	].join('\n'))
+	.demand(1)
+	.default('conn', 2)
+	.default('proxy', '')
+	.argv;
 var SITES = argv._;
+var isImage = /\.jpg|\.png|\.gif|\.pdf/i;
 
-SITES.forEach(traverseSite);
+var tarantula = new Tarantula({
+	leg: (argv.phantom ? 'PhantomJS' : ''),
+	legs: argv.legs,
+	stayInRange: !argv.web,
+	proxy: argv.proxy,
+});
+tarantula.shouldVisit = function (pageUri) {
+	if (isImage.test(pageUri)) {
+		return false;
+	}
+	return true;
+};
+tarantula.on('request', function (task) {
+	console.log('GET', task.uri);
+	console.log('REFER', task.parent);
+});
+tarantula.on('data', function (task) {
+	console.info('200', task.uri);
+});
 
-function traverseSite (site) {
-	console.info("Crawling… ", site);
-	// 2. Set-up the Crawler
-	var crawler = new Crawler({
-		maxConnections: MAX_CONNECTIONS,
-		callback: onResponse
-	});
-	var uris = {site: true};
-	var isSite = new RegExp(site);
-	var stripAnchors = new RegExp("#.+");
-	var queueLength = 1;
-	var completed = 0;
-
-	// 3. Queue up the URL as the first one to crawl
-	crawler.queue(site);
-
-	// 4. Handle any Errors that come through
-	// TODO: Need to find a way to know what URL we're talking about
-	/**
-	 * @param {Request.Error} error
-	 *   See Node Request module
-	 * @param {Request} result
-	 *   See Node Request module
-	 * @param {jQuery} $
-	 *   A jQuery instance scoped to the server-side DOM of the page
-	 */
-	function onResponse (error, result, $) {
-		queueLength--;
-		completed++;
-		if (error) {
-			if (error.length && error[0] && error[0].message) {
-				console.error(completed + ". " + "Error", error[0].message);
-			}
-			else {
-				console.error(completed + ". " + "Error", error);
-			}
-		}
-		else if (!result) {
-			console.error(completed + ". " + "Error: No result");
-		}
-		else if (!$) {
-			console.error(completed + ". " + result.statusCode + ": " + result.uri);
-			console.error("Error: Failed to load jQuery object");
-			// console.error(result);
-		}
-		else {
-			onSuccess(result, $);
+tarantula.on('uris', function (task, newCount) {
+	console.log(
+		'V:' + tarantula.visited,
+		'T:' + tarantula.uris.length,
+		'Q:' +  (tarantula.uris.length - tarantula.visited),
+		'+' + newCount
+	);
+});
+tarantula.on('error', function (task, errorCode, errorMessage) {
+	if (typeof errorMessage === 'string' && errorMessage.match('Not HTML')) {
+		console.warn('Not HTML');
+	}
+	else {
+		console.error(errorCode, task.uri, 'from', task.parent);
+		if (errorCode == 'ERR') {
+			console.error(errorMessage);
 		}
 	}
+});
+tarantula.on('done', function () {
+	console.info('done');
+});
 
-	// 5. Now handle responses that look good enough to crawl
-	/**
-	 * @param {Request} result
-	 *   See Node Request module
-	 * @param {jQuery} $
-	 *   A jQuery instance scoped to the server-side DOM of the page
-	 */
-	function onSuccess (result, $) {
-		console.info(completed + ". " + result.statusCode + ": " + result.uri);
-
-		var numLinks = 0;
-		// 6. Gather up the links on this page, and queue them for crawling
-		$("a").get().forEach(function(anchor) {
-			var uri = anchor.href.replace(stripAnchors, '');
-			if (!uris[uri] && isSite.test(uri)) {
-				crawler.queue(uri);
-				uris[uri] = true;
-				numLinks++;
-				queueLength++;
-			}
-		});
-		if (numLinks) {
-			console.info("Added", numLinks, "to the queue of", queueLength, "links");
-		}
-		else {
-			console.info("No links to add");
-		}
-	}
-}
-
-
+console.log('Crawling… ', SITES);
+tarantula.start(SITES);
